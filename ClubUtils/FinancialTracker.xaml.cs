8a5df5add88aacb54f912ded32427a167bc80c01
7AB7 <44379920+7AB7@users.noreply.github.com>
2018-11-12 15:45:41 -0500

    Financial Tracker Work
    Financial Tracker work

--- ClubUtils/FinancialTracker.xaml.cs
+++ ClubUtils/FinancialTracker.xaml.cs
@@ -23,5 +23,87 @@ namespace ClubUtils
         {
             InitializeComponent();
         }
+        private void ButtonClick1(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            tb.Text += b.Content.ToString();
+        }
+
+        private void ResultClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                result();
+            }
+            catch (Exception exc)
+            {
+                tb.Text = "Error!";
+            }
+        }
+
+        private void result()
+        {
+            String op;
+            int iOp = 0;
+            if (tb.Text.Contains("+"))
+            {
+                iOp = tb.Text.IndexOf("+");
+            }
+            else if (tb.Text.Contains("-"))
+            {
+                iOp = tb.Text.IndexOf("-");
+            }
+            else if (tb.Text.Contains("*"))
+            {
+                iOp = tb.Text.IndexOf("*");
+            }
+            else if (tb.Text.Contains("/"))
+            {
+                iOp = tb.Text.IndexOf("/");
+            }
+            else
+            {
+                //error    
+            }
+
+            op = tb.Text.Substring(iOp, 1);
+            double op1 = Convert.ToDouble(tb.Text.Substring(0, iOp));
+            double op2 = Convert.ToDouble(tb.Text.Substring(iOp + 1, tb.Text.Length - iOp - 1));
+
+            if (op == "+")
+            {
+                tb.Text += "=" + (op1 + op2);
+            }
+            else if (op == "-")
+            {
+                tb.Text += "=" + (op1 - op2);
+            }
+            else if (op == "*")
+            {
+                tb.Text += "=" + (op1 * op2);
+            }
+            else
+            {
+                tb.Text += "=" + (op1 / op2);
+            }
+        }
+
+        private void OffClick1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void DelClick(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "";
+        }
+
+        private void RClick(object sender, RoutedEventArgs e)
+        {
+            if (tb.Text.Length > 0)
+            {
+                tb.Text = tb.Text.Substring(0, tb.Text.Length - 1);
+            }
+        }
     }
 }
