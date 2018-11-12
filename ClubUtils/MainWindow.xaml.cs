2534ce5cb5dc48792a453e81c2c87eafb1790f21
Craigory V Coppola <craigorycoppola@gmail.com>
2018-11-12 10:35:29 -0500

    Calendar Beginnnings

--- ClubUtils/MainWindow.xaml.cs
+++ ClubUtils/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace ClubUtils
         {
             InitializeComponent();
             welcomeLabel.Content += Globals.currentMember.fullName;
+            myCalendar.BlackoutDates.Add(new CalendarDateRange(DateTime.MinValue, DateTime.Now.AddDays(-1)));
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
